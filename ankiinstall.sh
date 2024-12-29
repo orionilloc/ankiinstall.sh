4254b2f487fc165040b9c40772a569e633a3513e
orionilloc <123118718+orionilloc@users.noreply.github.com>
2024-12-28 21:54:14 -0500

    Refactoring ankiinstall.sh
    Added a case statement to accommodate other Linux distributions and made the initial install discovery a little more robust

--- ankiinstall.sh
+++ ankiinstall.sh
@@ -5,15 +5,35 @@
 current_anki_release=$(curl -v  https://github.com/ankitects/anki/releases/latest 2>&1  | grep "location" | cut -d ' ' -f 3 | awk -F / '{print $NF}'| tr -d '\r')
 
 # Ensures that Anki isn't already installed on this machine.
-if [ -f usr/local/bin/anki ]; then
-	echo "Anki already exists on this machine!" && exit 1
+if command -v anki &> /dev/null; then
+    echo "Anki is already installed on this machine!"
 fi
 
-# Installs dependencies for Anki.
-sudo apt install libxcb-xinerama0 libxcb-cursor0 zstd
+# Installs dependencies based on the distribution.
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    case "$ID" in
+        ubuntu|debian)
+            sudo apt update && sudo apt install -y libxcb-xinerama0 libxcb-cursor0 zstd
+            ;;
+        fedora)
+            sudo dnf install -y libxcb-xinerama0 libxcb-cursor0 zstd
+            ;;
+        arch)
+            sudo pacman -Syu --noconfirm libxcb xcb-util-cursor zstd
+            ;;
+        *)
+            echo "Unsupported distribution found: $ID"
+            ;;
+    esac
+else
+    echo "Unable to detect the OS. Please install dependencies manually."
+fi
 
-# Moves shell to the Downloads directory and grabs the latest Anki release via the wget utility.
+# Moves shell to the Downloads directory and grabs the latest Anki release.
 cd ~/Downloads && wget https://github.com/ankitects/anki/releases/download/"$current_anki_release"/anki-"$current_anki_release"-linux-qt6.tar.zst
 
-# Untars the file, changes directory to execute the installation shell script.
-tar xaf Downloads/anki-"$current_anki_release"-linux-qt6.tar.zst && cd anki-"$current_anki_release"-linux-qt6 && sudo ./install.sh
+# Untars the file, changes directory to installation files, executes the installation shell script.
+tar xaf anki-"$current_anki_release"-linux-qt6.tar.zst && cd anki-"$current_anki_release"-linux-qt6 && sudo ./install.sh
+
+echo "Anki installation complete!"
